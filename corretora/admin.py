0d75201c43ac7268ccecbd7b1f1b733d8574127b
Rodrigo Lins <rodrigolins@protonmail.com>
2020-07-03 15:10:53 -0300

    Adding admin register on apps

--- corretora/admin.py
+++ corretora/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Corretora
+
 # Register your models here.
+
+admin.site.register(Corretora)
--- corretora/models.py
+++ corretora/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Corretora(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     taxa_acoes = models.FloatField()
+    taxa_acoes_fracionario = models.FloatField()
     taxa_fiis = models.FloatField()
     taxa_etfs = models.FloatField()
 
